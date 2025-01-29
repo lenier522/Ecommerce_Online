ab753221f11de186a402f012654926cb7a43f50f
Lenier <59421605+lenier522@users.noreply.github.com>
2025-01-29 10:24:58 -0500

    Update UserController.java

--- src/main/java/Ecommerce_Online/Ecommerce/controller/UserController.java
+++ src/main/java/Ecommerce_Online/Ecommerce/controller/UserController.java
@@ -28,6 +28,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+/*
+*
+*@autor Lenier
+*
+*/
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
